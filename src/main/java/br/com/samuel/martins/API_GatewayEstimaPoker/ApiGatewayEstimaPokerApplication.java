a18372ab1827d3ed322bf2acdbac69d047ff8354
Samuel Martins <samuel.mrtins2016@gmail.com>
2024-12-16 22:13:09 -0300

    FIST COMMIT

---
+++ src/main/java/br/com/samuel/martins/API_GatewayEstimaPoker/ApiGatewayEstimaPokerApplication.java
@@ -0,0 +1,13 @@
+package br.com.samuel.martins.API_GatewayEstimaPoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayEstimaPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayEstimaPokerApplication.class, args);
+	}
+
+}
